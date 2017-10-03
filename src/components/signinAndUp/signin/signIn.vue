032ea7fdd36f1122ecea25e08a6faf2ed9a0d71b
zhuL <1161107512@qq.com.com>
2017-10-03 14:24:50 +0800

    登录页面

---
+++ src/components/signinAndUp/signin/signIn.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import './signIn.scss'
+    export default{
+        data(){
+            return {}
+        },
+        components: {},
+        created(){
+        },
+        mounted(){
+        },
+        methods: {}
+    }
+</script>
---
+++ src/components/signinAndUp/signup/signUp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import './signUp.scss'
+    export default{
+        data(){
+            return {}
+        },
+        components: {},
+        created(){
+        },
+        mounted(){
+        },
+        methods: {}
+    }
+</script>

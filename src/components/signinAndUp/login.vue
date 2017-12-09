69bcd6aacfc658675f5edad14b0ed8976845396c
zhuL <1161107512@qq.com.com>
2017-12-09 21:27:45 +0800

    功能页面

--- src/components/signinAndUp/login.vue
+++ src/components/signinAndUp/login.vue
@@ -12,7 +12,7 @@
         <!--<div class="zhdl">
           <span style="margin-left:38% ;font-size:20px;color: white;">账户登陆</span>
         </div>-->
-        <div id="inputvalue" style="width: 100%">
+        <div id="inputvalue" style="width: 100%;position: relative;">
           <div class="inputgroup">
             <div style="width: 100%;  display: flex;justify-content: left;align-items: center;margin-top: 5%;margin-left: 22%">
               <Icon class="ivu-icon ivu-icon-person" size="25"></Icon>&nbsp&nbsp
@@ -24,17 +24,14 @@
             <Input type="password" style=" width: 50%" class="inputtext"     v-model="password" id="password"
                    placeholder="密码"/>
             </div>
+            <div class="wjmm">
+              <router-link to="/xgpassword/first"  style="margin-left: 68%;margin-top: 5%">忘记密码</router-link>
+            </div>
+            <Button type="info" @click="login" class="btn1" id="dl" style="margin-top: 3% ; font-size: 17px;width: 53% ;background: #6a4f90;border-color: #6a4f90;height:40px;margin-left: 22%">登录
+            </Button>
           </div>
 
-          <div class="wjmm">
-            <router-link to="/xgpassword/first"  style="margin-left: 60%;margin-top: 5%">忘记密码</router-link>
-          </div>
-          <Button @click="login" class="btn1" id="dl" style="margin-top: 8% ;width:60%; background: #BADCDB; color: white; font-size:15px;margin-left: 22%">登录
-          </Button>
-          <div class="registmdl">
-            <img class="jt" src="../../assets/img/redarrow.jpg"/>
-            <router-link to="/regist" class="regist">前往注册</router-link>
-          </div>
+
         </div>
 
 
@@ -50,24 +47,6 @@
 </template>
 
 <style>
-  .top {
-    height: 15%;
-    width: 100%;
-
-  }
- .close-item i{
-   font-size: 30px;
-   color: white;
- }
-  .close-item{
-    width:30px;
-    position: absolute;
-    top:10px;
-    right: 10px;
-  }
-  .close-item:hover{
-    transform: rotate(180deg);
-  }
 
   #main {
     width: 100%;
@@ -77,13 +56,7 @@
     MARGIN-LEFT: auto;
   }
 
-  .icon {
-    width: 40%;
-    height: 100%;
-    float: left;
-    margin-top: 2%;
 
-  }
 
   .ivu-icon ivu-icon-person myicon1 {
        margin-left: 20%;
@@ -94,14 +67,6 @@
 
   }
 
-  .stroke {
-    margin-left: 30px;
-    color: transparent;
-    -webkit-text-stroke: 1px black;
-    letter-spacing: 0.04em;
-    color: orange;
-
-  }
 
   .mdl {
     height: 320px;
@@ -110,19 +75,6 @@
     top:22%;
 /*    display: flex;
     justify-content: center;*/
-  }
-
-  .zhdl {
-    width: 100%;
-    height: 10%;
-    background: #BADCDB;
-    padding-top: 2%;
-  }
-
-  .username {
-    width: 80%;
-    margin-top: 2px;
-    height: 35px;
   }
 
   .inputgroup {
@@ -152,28 +104,6 @@
 
   }
 
-  .regist {
-    float: left;
-    margin-top: 2px;
-
-    font-size: 20px;
-    text-decoration: none;
-    color: #f6387E;
-  }
-
-  .jt {
-    margin-left: 48%;
-    float: left;
-    width: 25px;
-    height: 25px;
-  }
-
-  .registmdl {
-    width: 100%;
-    height: 10%;
-    margin-top: 5%;
-
-  }
 </style>
 
 <script>
